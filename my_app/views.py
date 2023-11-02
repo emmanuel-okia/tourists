d72939f85397c405981773cc9d7e784156ba7044
Emma <okiaemmanuel304@gmail.com>
2023-11-02 09:31:33 +0300

    crud.models

--- my_app/views.py
+++ my_app/views.py
@@ -5,6 +5,12 @@ from my_app.forms import WildForm
 from my_app.forms import VisitorForm
 from my_app.forms import PaymentForm
 
+from my_app.models import Warden
+from my_app.models import Game_park
+from my_app.models import Wild
+from my_app.models import Visitor
+from my_app.models import Payment
+
 
 
 
@@ -24,82 +30,110 @@ def game_park_view(request):
 
 
 def add_warden_view(request):
+    message = ''
     if request.method == "POST":
         warden_form = WardenForm(request.POST)
 
         if warden_form.is_valid():
             warden_form.save()
 
+            message = "addition successful"
+
     else:
         warden_form = WardenForm()
+    
+    warden = Warden.objects.all()
 
 
     context ={
         'form':warden_form,
+        'msg':message,
+        'warden': warden,
     }
     return render(request, "add_warden.html", context)
 
 def add_game_park_view(request):
+    message = ''
     if request.method == "POST":
         game_park_form = Game_parkForm(request.POST)
 
         if game_park_form.is_valid():
             game_park_form.save()
+            message = "addition successful"
 
     else:
         game_park_form = Game_parkForm()
+        game_park = Game_park.objects.all()
 
 
     context ={
         'form':game_park_form,
+        'msg':message,
+        'g_park': game_park,
     }
     return render(request, 'add_game_park.html', context)
 
 def add_wild_view(request):
+    message = ''
     if request.method =="POST":
         wild_form = WildForm(request.POST)
 
         if wild_form.is_valid():
             wild_form.save()
+            message = "addition successful"
 
     else:
         wild_form = WildForm()
+        wild = Wild.objects.all()
 
     context ={
         'form':wild_form,
+        'msg':message,
+        'wild': wild
     }
 
     return render(request, "add_wild.html", context)
 
 def add_visitor_view(request):
+    message = ''
     if request.method =="POST":
         visitor_form = VisitorForm(request.POST)
 
         if visitor_form.is_valid():
             visitor_form.save()
+            message = "addition successful"
 
     else:
         visitor_form = VisitorForm()
+        visitor = Visitor.objects.all()
 
     context ={
         'form':visitor_form,
+        'msg':message,
+        'vst': visitor
     }
 
     return render(request, "add_visitor.html", context)
 
 
 def add_payment_view(request):
+    message = ''
     if request.method =="POST":
         payment_form = PaymentForm(request.POST)
 
         if payment_form.is_valid():
             payment_form.save()
 
+            message = "addition successful"
+
     else:
         payment_form = PaymentForm()
+        payment = Payment.objects.all()
 
     context ={
         'form':payment_form,
+        'msg':message,
+        'pyt': payment
     }
 
     return render(request, "add_payment.html", context)
